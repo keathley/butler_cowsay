25b7985308ac262ab9a0fa42551452a4420e376e
Bill Brock <william.e.brock@gmail.com>
2015-12-04 23:30:07 -0500

    bumped version

--- lib/butler_cowsay.ex
+++ lib/butler_cowsay.ex
@@ -1,6 +1,9 @@
 defmodule Butler.Cowsay do
   use Butler.Plugin
 
+  @usage """
+  returns your text input as a cow
+  """
   respond(~r/cowsay (.*)$/, conn, [_all, say]) do
     resp_string = say |> Cowsay.say
 
